4d5ddb4da015a123c5a5d8bbfc2b709add16c124
nona mobile <edith24si@mahasiswa.pcr.ac.id>
2026-04-17 07:58:25 +0700

    Pertemuan 4: warming up

--- app/src/main/java/com/example/thereapps/MainActivity.kt
+++ app/src/main/java/com/example/thereapps/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.thereapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.thereapps.databinding.ActivityFourthBinding
+import com.example.thereapps.databinding.ActivityMainBinding
+import com.example.thereapps.pertemuan_4.FourthActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:   ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set onClickListener
+        binding.btnKirim.setOnClickListener {
+            val intent = Intent(this, FourthActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
